289f55c9283a73ec99b04c30ae44c3d7ec0f4c72
Santi Rojo <santi.rojo.franco@gmail.com>
2023-03-28 01:06:37 -0300

    Detalles de maquetación y creación de app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const frontContainer = document.querySelector('.front-container');
+const backContainer = document.querySelector('.back-container');
+
+const inputPromotores = document.querySelector('#input-promotores');
+const inputNeutros = document.querySelector('#input-neutros');
+const inputDetractores = document.querySelector('#input-detractores');
+
+let promotores = inputPromotores.value;
+let neutros = inputNeutros.value;
+let detractores = inputDetractores.value;
