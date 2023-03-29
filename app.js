de38969091197758d02139952bc0723a1bf11456
Santi Rojo <santi.rojo.franco@gmail.com>
2023-03-28 21:03:41 -0300

    Finalización de maquetado y estilado, desarrollo js

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const inputPromotores = document.querySelector('#input-promotores');
 const inputNeutros = document.querySelector('#input-neutros');
 const inputDetractores = document.querySelector('#input-detractores');
 
+const containerTituloDesempenio = document.querySelector('#container-titulo-card');
+const tituloCard = document.querySelector('#titulo-card');
+const containerPorcentaje = document.querySelector('#container-porcentaje');
+const porcentaje = document.querySelector('#porcentaje');
+
+const mensaje = document.querySelector('#mensaje');
+const mensajeObjetivo = document.querySelector('#mensaje-objetivo');
+
+const calcButton = document.querySelector('#calc-button');
+const calcular = document.querySelector('#calcular');
+const cerrar = document.querySelector('#cerrado');
 
 let promotores;
 let neutros;
@@ -12,13 +23,25 @@ let detractores;
 
 
 inputPromotores.addEventListener('change', () => {
-    promotores = inputPromotores.value;
+    promotores = parseInt(inputPromotores.value);
+    console.log(promotores)
 });
 
 inputNeutros.addEventListener('change', () => {
-    neutros = inputNeutros.value;
+    neutros = parseInt(inputNeutros.value); 
+    console.log(neutros)
 });
 
 inputDetractores.addEventListener('change', () => {
-    detractores = inputDetractores.value;
+    detractores = parseInt(inputDetractores.value);
+    console.log(detractores)
+});
+
+
+calcButton.addEventListener('click', () => {
+    frontContainer.classList.toggle('front-rotate');
+    backContainer.classList.toggle('back-rotate');
+    calcButton.classList.toggle('cerrar');
+    calcular.classList.toggle('inactive');
+    cerrar.classList.toggle('inactive');
 });
